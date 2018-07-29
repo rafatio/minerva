ddd4993d186de8e431f90032d7fac07924266741
Flavio Wuensche <flavio@touts.com.br>
2018-07-29 19:23:43 -0300

    Add mockup of mentors and payments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: "users#show"
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :payments, only: :index
+  resources :mentors, only: :index
+  resources :users, only: :show
 end
