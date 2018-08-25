5c788cea4dc59349ba2a67cb60bb90acee672435
Flavio Wuensche <flavio@touts.com.br>
2018-08-25 19:19:42 -0300

    Remove rememberable from devise components

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :rememberable, and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :trackable, :validatable
 
   has_many :payments
 end
