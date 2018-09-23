1acec58c3c842a1674a82fb3443689d0fe5d0f53
Rafael Rozendo <rafael.g.rozendo@gmail.com>
2018-09-23 18:57:21 -0300

    visual and minor adjustments: * changing some error and success messages * changing site title to "Fundo Minerva" * add sort function to payments table

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :payments, only: [:index, :new, :create]
-  resources :mentors, only: :index
+  #resources :mentors, only: :index
 end
