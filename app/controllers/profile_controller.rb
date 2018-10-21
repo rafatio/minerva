30009483b97f27665fc1c268ab4ed327420a773e
Rafael Rozendo <rafael.g.rozendo@gmail.com>
2018-10-21 19:16:42 -0300

    CRM views

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,18 @@
 class ProfileController < ApplicationController
     before_action :authenticate_user!
     def index
+        @person = current_user.person
+        @contacts = current_user.contacts
+        @address = current_user.address
+        @professional_information = current_user.professional_information
+        @education_information = current_user.education_information
+        @intended_relationship = current_user.intended_relationship
+
+        @countries = Country.all
+    end
+
+    def create
+        flash[:notice] = 'Perfil atualizado com sucesso'
+        redirect_to profile_index_path
     end
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,2 @@
 Time::DATE_FORMATS[:custom_datetime] = '%d/%m/%Y - %H:%M'
+Time::DATE_FORMATS[:short_date] = '%Y-%m-%d'
