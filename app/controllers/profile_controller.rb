70fc8aee3898b664e0cba7e8c3e05604675ba308
Rafael Rozendo <rafael.g.rozendo@gmail.com>
2018-10-28 16:16:20 -0300

    post person

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,5 @@
 class ProfileController < ApplicationController
+
     before_action :authenticate_user!
     def index
         @person = current_user.person
@@ -12,7 +13,26 @@ class ProfileController < ApplicationController
     end
 
     def create
+        if current_user.person.nil?
+            @person = Person.new
+        else
+            @person = current_user.person
+        end
+
+        User.transaction do
+            @person.name = params['person-name']
+            @person.gender = params['person-gender']
+            @person.birth_date = params['person-birthdate']
+            @person.cpf = params['person-cpf']
+            @person.rg = params['person-rg']
+            current_user.person = @person
+        end
+
         flash[:notice] = 'Perfil atualizado com sucesso'
         redirect_to profile_index_path
+    rescue => e
+        flash[:error] = e.message
+        redirect_to profile_index_path
+
     end
 end
