9848c3c6efd7080bade0baa7ff99ac3108576d96
Bernardo Amorim <contato@bamorim.com>
2020-04-18 18:14:48 -0300

    test: add code coverage report

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start 'rails'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+Rails.application.eager_load!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
